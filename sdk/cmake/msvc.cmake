50139ed8926a3d29057f75e4ee2ab816365fb987
Maj <majs.1236@gmail.com>
2024-04-18 16:59:11 +0200

    actually compile AVX2, now we pray

--- sdk/cmake/msvc.cmake
+++ sdk/cmake/msvc.cmake
@@ -55,7 +55,7 @@ if(ARCH STREQUAL "i386")
     if(CMAKE_C_COMPILER_ID STREQUAL "Clang")
         add_compile_options(-march=${OARCH})
     else()
-        add_compile_options(/arch:IA32)
+        add_compile_options(/arch:AVX2)
     endif()
 endif()
 
